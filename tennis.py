46da8578692221d9e0df98ac82284c235fe5527f
Dmitry Kagan <dmitrykag@gmail.com>
2022-08-12 22:01:12 +0300

    причешем

--- tennis.py
+++ tennis.py
@@ -1,5 +1,4 @@
 import pygame
-import random
 
 WIDTH = 800
 HEIGHT = 600
@@ -11,11 +10,13 @@ class Ball:
     def __init__(self):
         self.image = pygame.image.load("small_tennis.png")
         self.image = pygame.transform.scale(self.image, (32, 32))
-        self.speed = [random.randrange(1,6), 3]
+        self.speed = [1, 1]
         self.rect = self.image.get_rect()
 
-    def update(self):
-        if self.rect.top < 0 or self.rect.bottom > HEIGHT:
+    def update(self, paddle_rect: pygame.Rect):
+        if self.rect.colliderect(paddle_rect):
+            self.speed[0] *= -1
+        elif self.rect.top < 0 or self.rect.bottom > HEIGHT:
             self.speed[1] *= -1
         elif self.rect.left < 0 or self.rect.right > WIDTH:
             self.speed[0] *= -1
@@ -29,21 +30,26 @@ class Paddle:
         self.rect = pygame.Rect(0, HEIGHT/2, 10, 60)
         self.rect.center = (self.rect.width/2, HEIGHT/2) 
 
-def main():
-    clock = pygame.time.Clock()
-    pygame.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+class Scene:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.cleanup()
+        self.create()
 
-    ball = Ball()
-    paddle = Paddle()
+    def cleanup(self):
+        self.ball = None
+        self.paddle = None
 
-    def runEventLoop() -> bool:
+    def create(self):
+        if not self.ball:
+            self.ball = Ball()
+            self.paddle = Paddle()
+
+    def pumpEvents(self) -> bool:
         for event in pygame.event.get():
             match event.type:
-                case pygame.MOUSEBUTTONDOWN:
-                    if ball.rect.collidepoint(pygame.mouse.get_pos()):
-                        ball.speed[0] = random.randrange(-4, 4)
-                        ball.speed[1] = -2
                 case pygame.QUIT:
                     return False
                 case pygame.KEYDOWN:
@@ -52,50 +58,51 @@ def main():
 
         keyState = pygame.key.get_pressed()
         if keyState[pygame.K_DOWN]:
-            paddle.rect.centery += 5
+            self.paddle.rect.centery += 3
         elif keyState[pygame.K_UP]:
-            paddle.rect.centery -= 5
+            self.paddle.rect.centery -= 3
         
         return True
 
-
-    b_run = True
-    game_over = False
-    game_over_timer = 60
-
-    while b_run:
-        screen.fill(BACKGROUND)
-
-        if game_over:
-            font = pygame.font.SysFont(None, 100)
-            text = font.render("Game over", True, RED)
-            screen.blit(text,(200,200))
-            game_over_timer -= 1
-            ball = None
-            pygame.display.flip()
-            clock.tick(60)
-            for event in pygame.event.get():
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_ESCAPE:
-                        game_over = False
-            b_run = runEventLoop()
-            continue
-
-        if not ball:
-            ball = Ball()
-
-        if ball.rect.colliderect(paddle.rect):
-            ball.speed[1] *= -1
-        elif ball.rect.left < 0:
-            game_over = True
-    
-        screen.blit(ball.image, ball.rect)
-        pygame.draw.rect(screen, WHITE, paddle.rect)
-        ball.update()
+    def game_over(self) -> bool:
+        font = pygame.font.SysFont('arial', 100)
+        text = font.render("Game over", True, RED)
+        self.screen.blit(text,(150,200))
+        self.cleanup()
         pygame.display.flip()
-        clock.tick(60)
-        b_run = runEventLoop()
+        self.clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    return False
+        return True
 
+    def play(self) -> None:
+        b_run = True
+        game_over = False
+        while b_run:
+            self.screen.fill(BACKGROUND)
+
+            if game_over:
+                game_over = self.game_over()
+                b_run = self.pumpEvents()
+                continue
+            else:
+                self.create()
+
+            if self.ball.rect.left < 0:
+                game_over = True
+        
+            self.screen.blit(self.ball.image, self.ball.rect)
+            pygame.draw.rect(self.screen, WHITE, self.paddle.rect)
+            self.ball.update(self.paddle.rect)
+            pygame.display.flip()
+            self.clock.tick(240)
+            b_run = self.pumpEvents()
+
+def main():
+    scene = Scene()
+    scene.play()
 
 if __name__ == "__main__":
     main()
